766f1315634c46b629d7c0378da128c1d6f9af59
Samonto Karmaker <samonto0726@gmail.com>
2026-03-22 18:22:06 +0600

    Add Testimonials section to showcase customer feedback

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Hero from "./sections/Hero"
 import LogoSection from "./sections/LogoSection"
 import Showcase from "./sections/Showcase"
 import TechStack from "./sections/TechStack"
+import Testimonials from "./sections/Testimonials"
 
 const App = () => {
     return (
@@ -16,6 +17,7 @@ const App = () => {
             <FeatureCards />
             <Experience />
             <TechStack />
+            <Testimonials />
         </>
     )
 }
