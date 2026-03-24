e84816ae8dada107752cbdb4c97918e9f10d723e
Samonto Karmaker <samonto0726@gmail.com>
2026-03-24 14:02:46 +0600

    Adjust scale of 3D model in HeroExperience for improved responsiveness

--- src/components/models/HeroExperience.tsx
+++ src/components/models/HeroExperience.tsx
@@ -72,7 +72,7 @@ const HeroExperience = () => {
                 <HeroLights />
                 <Particles count={isMobile ? 40 : isTablet ? 70 : 100} />
                 <group
-                    scale={isMobile ? 0.7 : 1}
+                    scale={isMobile ? 0.5 : 1}
                     position={[0, -3.5, 0]}
                     rotation={[0, -Math.PI / 4, 0]}
                 >
